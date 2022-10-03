b00a23107fc6e3b4c9e04863f2696ea023c6c34b
Martin <martin@email.com>
2022-10-03 13:37:01 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationExtras, Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  listefilm = []
+  constructor(private router: Router){
+    fetch('./assets/films-json.json').then(res => res.json()).then(json => {
+      this.listefilm = json;
+    })
+  }
+  
+  affDetail(item){
+    let navigationExtras: NavigationExtras = {
+      state : {
+        film : item
+      }
+    }
+    console.log(item)
+    //ouvrir page detail
+    //envoyer le param a la page detail
+    this.router.navigate(['/details'],navigationExtras);
+  }
 
+  
+  
 }
